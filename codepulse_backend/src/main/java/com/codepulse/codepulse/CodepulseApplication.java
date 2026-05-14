1e023a200ccbb2328ec150ed13286d18c69cdd55
Dnyaneshwari_Garole <101723227+DnyaneshwariGarole@users.noreply.github.com>
2026-05-14 12:29:20 +0530

    Format main method for improved readability

--- codepulse_backend/src/main/java/com/codepulse/codepulse/CodepulseApplication.java
+++ codepulse_backend/src/main/java/com/codepulse/codepulse/CodepulseApplication.java
@@ -6,7 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CodepulseApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		SpringApplication.run(CodepulseApplication.class, args);
 	}
 
